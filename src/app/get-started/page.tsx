125d2a5534a73a6132317acfaee2b91732869264
ishanvepa <ishanvepa171@gmail.com>
2025-01-14 14:00:29 -0500

    added google and spotify login icons

--- src/app/get-started/page.tsx
+++ src/app/get-started/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import Image from "next/image";
 import React from 'react';
 import Link from 'next/link';
-import Typewriter from 'typewriter-effect';
+import {Input} from "@nextui-org/input";
+import { FcGoogle } from "react-icons/fc";
+import { FaSpotify } from "react-icons/fa";
+
 
 export default function getStarted() {
   return (
@@ -11,8 +13,19 @@ export default function getStarted() {
 
       <div className="flex flex-col items-center justify-center h-screen">        
         
-        <div className="flex text-2xl sm:text-4xl"> {/* wrapper for header text with typewriter effect */}
+        <div className="text-2xl sm:text-4xl"> 
           <h3>get started</h3>
+
+          <div className="flex gap-10 mt-10 justify-center">
+            <Link href="google-auth">
+              <FcGoogle style={{scale: '1.5'}}/>
+            </Link>
+
+            <Link href="spotify-auth">
+              <FaSpotify style = {{fill: '#21d760', scale: '1.5'}}/>
+            </Link>
+          </div>
+          
         </div>
 
       </div>
