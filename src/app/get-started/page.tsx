9c1f05d07d4b506c1c7d0adedfc57b91359fc7b5
ishanvepa <ishanvepa171@gmail.com>
2025-01-14 13:36:09 -0500

    init get-started page/branch

---
+++ src/app/get-started/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Image from "next/image";
+import React from 'react';
+import Link from 'next/link';
+import Typewriter from 'typewriter-effect';
+
+export default function getStarted() {
+  return (
+    <div>
+
+      <div className="flex flex-col items-center justify-center h-screen">        
+        
+        <div className="flex text-2xl sm:text-4xl"> {/* wrapper for header text with typewriter effect */}
+          <h3>get started</h3>
+        </div>
+
+      </div>
+
+
+    </div>
+      
+
+      
+    
+
+    
+  );
+}
